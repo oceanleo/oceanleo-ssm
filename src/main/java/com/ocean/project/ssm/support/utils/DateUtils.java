edad1932866aec6b111f3162d8f73f292255295e
haiyang.li <275564847@qq.com>
2017-08-24 15:57:52 +0800

    添加注解

--- src/main/java/com/ocean/project/ssm/support/utils/DateUtils.java
+++ src/main/java/com/ocean/project/ssm/support/utils/DateUtils.java
@@ -24,7 +24,6 @@ public class DateUtils {
 
     public static String format(Date date, String format) {
         if (StringUtils.hasText(format) && date != null) {
-
             return new SimpleDateFormat(format).format(date);
         }
         return StringUtils.EMPTY;
--- src/main/java/com/ocean/project/ssm/support/utils/StringUtils.java
+++ src/main/java/com/ocean/project/ssm/support/utils/StringUtils.java
@@ -7,6 +7,8 @@ public class StringUtils {
 
     public static final String EMPTY = "";
 
+    public static final String SEPARATOR = ",";
+
     public static boolean hasText(String str){
         return org.springframework.util.StringUtils.hasText(str);
     }
